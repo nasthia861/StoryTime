3c9617c542592abb5c4abd9acd19677b868505ce
yeauxdejuan <dejuan.ells@gmail.com>
2023-09-09 10:11:40 -0500

    pulling main/updating route file  locaiton

--- server/database /upVote.js
+++ server/database /upVote.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const Text = require()
+const {Text} = require('./index')
 
 
-router.put('/likes')
+router.post('/text/:textId/likes', (req, res) => {
+  const { id } = req.params;
+  const { action } = req.body;
+//find post by primary key
+  Text.findByPk(id)
+  .then((post) => {
+    //validate post exist => send 404 otherwise
+    if (!post) {
+      res.status(404).json({error: 'Post not Found'});
+    } else if ( action === 'like') {
+      //access like property from post object and increment count
+      post.likes += 1
+    }
+
+  })
+})
