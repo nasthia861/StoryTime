316b3c7a62d921fa7c590eddaa3f18b0317f7202
abell10101 <ajbell1194@gmail.com>
2023-09-09 14:19:17 -0500

    homepage syntax change

---
+++ client/components/Homepage.jsx
@@ -0,0 +1,69 @@
+import React, {useState, useEffect} from 'react';
+import axios from'axios';
+
+function Homepage() {
+  //setting states of genrated word, current story, and input using hooks
+  const [words, setWords] = useState(['alleviate', 'run', 'no', 'why', 'infallible'])
+  const [story, setStory] = useState(['Why run to alleviate infallible pain?'])
+  const [input, setInput] = useState('')
+
+  //useEffect to fetch data from database upon mounting
+
+  // useEffect(() => {
+  //   axios.get('/api/words')
+  //   .then(response => {
+  //     setWords(response.data)
+  //   })
+  //   .catch(err => {
+  //     console.error('Error getting words:', err)
+  //   })
+  // }, [])
+
+  //funciton to handle input change
+  const handleInput = (event) => {
+    setInput(event.target.value)
+  }
+
+  //function to handle user submut
+  const handleSubmit = () => {
+    //sets story to current story plus users input
+    setStory(`${story}\n ${input}`)
+  }
+
+  
+  //return dom elements and structure
+  return (
+    //div for wrapper containing all homepage elements
+    <div className='wrapper'>
+      <div className='word-container'>
+        {words.map((word, i) => (
+        <span key={i}>{word } </span>
+      ))}
+      </div>
+      
+        <div className='story-container'>
+          <p>{story}</p>
+        </div>
+
+        <div>
+          
+          
+          <input 
+          className='user-input'
+          type='text'
+          placeholder='Add to the story!' 
+          onChange={handleInput}
+          value={input}
+          />
+          <div className='submit'>
+          <button className='submit-btn' onClick={handleSubmit}>Submit</button>
+          </div>
+
+        </div>
+
+    </div>
+    
+  )
+};
+
+export default Homepage
